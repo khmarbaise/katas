6e293e9493f928e7008877947f871be02fd7aa91
Karl Heinz Marbaise <info@soebes.de>
2011-12-02 22:10:54 +0100

    - Added Code Kata for GetraenkeAutomat.

---
+++ src/test/java/com/soebes/katas/getraenkeautomat/GetraenkeAutomatTest.java
@@ -0,0 +1,15 @@
+package com.soebes.katas.getraenkeautomat;
+
+import org.testng.annotations.Test;
+
+public class GetraenkeAutomatTest {
+
+    @Test
+    public void firstTest() {
+        //Getränke Automat
+        // Geld Einwurf mit Münzspeicher (Geldwechsler)
+        // Zustände des Automaten
+        // Ausgabe Getränk...
+        //
+    }
+}
