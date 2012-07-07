26bf8a7afa46acef5c64a2dc862291cca3b8457d
Karl Heinz Marbaise <info@soebes.de>
2012-07-07 17:15:04 +0200

    - Added Requirements for vending machine - Updated pom to new smpp version (0.5)   and cleaned up the pom.

---
+++ src/test/java/com/soebes/katas/filmausleihe/FilmeTest.java
@@ -0,0 +1,10 @@
+package com.soebes.katas.filmausleihe;
+
+public class FilmeTest {
+
+    //Mehrere unterschiedlicher Filme
+    //Unterschiedliche Preise (je nach Typ und Ausleidauer)
+    //Unterschiedliche Personen
+    //Rechnungsausgabe
+
+}
