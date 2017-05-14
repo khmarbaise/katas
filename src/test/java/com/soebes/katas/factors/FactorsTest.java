64285788d76176b97bffaa5e4ab91c0fb48e00c0
Karl Heinz Marbaise <khmarbaise@apache.org>
2017-05-14 12:31:50 +0200

    start position.

--- src/test/java/com/soebes/katas/factors/FactorsTest.java
+++ src/test/java/com/soebes/katas/factors/FactorsTest.java
@@ -3,7 +3,6 @@ package com.soebes.katas.factors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,38 +14,10 @@ public class FactorsTest
     public void nothing()
     {
         assertThat( factorsOf( 1 ) ).isEqualTo( Collections.emptyList() );
-        assertThat( factorsOf( 2 ) ).containsExactly( 2 );
-        assertThat( factorsOf( 3 ) ).containsExactly( 3 );
-        assertThat( factorsOf( 4 ) ).containsExactly( 2, 2 );
-        assertThat( factorsOf( 5 ) ).containsExactly( 5 );
-        assertThat( factorsOf( 6 ) ).containsExactly( 2, 3 );
-        assertThat( factorsOf( 7 ) ).containsExactly( 7 );
-        assertThat( factorsOf( 8 ) ).containsExactly( 2, 2, 2 );
-        assertThat( factorsOf( 9 ) ).containsExactly( 3, 3 );
-        assertThat( factorsOf( 10 ) ).containsExactly( 2, 5 );
-        assertThat( factorsOf( 11 ) ).containsExactly( 11 );
-        assertThat( factorsOf( 12 ) ).containsExactly( 2, 2, 3 );
-        assertThat( factorsOf( 13 ) ).containsExactly( 13 );
-        assertThat( factorsOf( 14 ) ).containsExactly( 2, 7 );
-        assertThat( factorsOf( 15 ) ).containsExactly( 3, 5 );
-        assertThat( factorsOf( 2 * 2 * 3 * 3 * 5 * 5 ) ).containsExactly( 2, 2, 3, 3, 5, 5 );
     }
 
     public List<Integer> factorsOf( final int n )
     {
-        List<Integer> result = new ArrayList<>();
-        int remainder = n;
-        int divider = 2;
-        while ( remainder > 1 )
-        {
-            while ( remainder % divider == 0 )
-            {
-                result.add( divider );
-                remainder /= divider;
-            }
-            
-            divider++;
-        }
-        return result;
+        return Collections.emptyList();
     }
 }
