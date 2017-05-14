5ebdbc0eb96dc42d3314675d13e9a9357c893c24
Karl Heinz Marbaise <khmarbaise@apache.org>
2017-05-14 12:44:16 +0200

    working with more example.

--- src/test/java/com/soebes/katas/factors/FactorsTest.java
+++ src/test/java/com/soebes/katas/factors/FactorsTest.java
@@ -20,14 +20,25 @@ public class FactorsTest
         assertThat( factorsOf( 4 ) ).containsExactly( 2, 2 );
         assertThat( factorsOf( 5 ) ).containsExactly( 5 );
         assertThat( factorsOf( 6 ) ).containsExactly( 2, 3 );
+        assertThat( factorsOf( 7 ) ).containsExactly( 7 );
+        assertThat( factorsOf( 8 ) ).containsExactly( 2, 2, 2 );
+        assertThat( factorsOf( 9 ) ).containsExactly( 3, 3 );
+        assertThat( factorsOf( 10 ) ).containsExactly( 2, 5 );
+        assertThat( factorsOf( 11 ) ).containsExactly( 11 );
+        assertThat( factorsOf( 12 ) ).containsExactly( 2, 2, 3 );
+        assertThat( factorsOf( 13 ) ).containsExactly( 13 );
+        assertThat( factorsOf( 14 ) ).containsExactly( 2, 7 );
+        assertThat( factorsOf( 15 ) ).containsExactly( 3, 5 );
+        assertThat( factorsOf( 15 ) ).containsExactly( 3, 5 );
+        assertThat( factorsOf( 2 * 2 * 3 * 3 * 5 * 5 ) ).containsExactly( 2, 2, 3, 3, 5, 5 );
     }
 
     public List<Integer> factorsOf( final int n )
     {
         List<Integer> result = new ArrayList<>();
-        
+
         int remainder = n;
-        
+
         if ( remainder > 1 )
         {
             while ( remainder % 2 == 0 )
@@ -35,13 +46,27 @@ public class FactorsTest
                 result.add( 2 );
                 remainder /= 2;
             }
-            if ( remainder % 3 == 0 )
+            while ( remainder % 3 == 0 )
             {
                 result.add( 3 );
+                remainder /= 3;
             }
-            if ( remainder % 5 == 0 )
+            while ( remainder % 5 == 0 )
             {
                 result.add( 5 );
+                remainder /= 5;
+            }
+            if ( remainder % 7 == 0 )
+            {
+                result.add( 7 );
+            }
+            if ( remainder % 11 == 0 )
+            {
+                result.add( 11 );
+            }
+            if ( remainder % 13 == 0 )
+            {
+                result.add( 13 );
             }
         }
         return result;
