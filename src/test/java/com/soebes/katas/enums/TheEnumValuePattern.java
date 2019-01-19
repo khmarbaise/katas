ec7036c8961af1af110662ba9c5600e1c39b6fc2
Karl Heinz Marbaise <khmarbaise@soebes.de>
2019-01-19 13:52:44 +0100

    Added TheEnumValuePattern test

---
+++ src/test/java/com/soebes/katas/enums/TheEnumValuePattern.java
@@ -0,0 +1,62 @@
+package com.soebes.katas.enums;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public enum TheEnumValuePattern {
+
+	/**
+	 * FIRST 
+	 */
+	FIRST(Values.FIRST), 
+	/**
+	 * SECOND 
+	 */
+	SECOND(Values.SECOND), 
+	/**
+	 * THIRD. 
+	 */
+	THIRD(Values.THIRD);
+
+	private final String value;
+
+	private TheEnumValuePattern(String value) {
+		this.value = value;
+	}
+
+	public final String getValue() {
+		return this.value;
+	}
+
+	public static final class Values {
+		private static final String PREFIX = "BEGINNING-";
+
+		public static final String FIRST = PREFIX + "First";
+		public static final String THIRD = PREFIX + "Third";
+		public static final String SECOND = PREFIX + "Second";
+
+		private Values() {
+			// prevent instantiation.
+		}
+	}
+
+	/**
+	 * @param value The {@link Values} which should be converted to the appropriate
+	 *             enum {@link TheEnumValuePattern}.
+	 * 
+	 * @return a value of {@link TheEnumValuePattern}
+	 * @throws {@link IllegalArgumentException} in case of an non existing text.
+	 */
+	public static final TheEnumValuePattern fromValue(final String value) {
+		return Arrays.stream(TheEnumValuePattern.values()) //
+				.filter(s -> s.getValue().equals(value)) //
+				.findAny() //
+				.orElseThrow(() -> new IllegalArgumentException("The given.."));
+	}
+	
+	public String validValues() {
+		return Arrays.stream(TheEnumValuePattern.values()) //
+				.map(s -> s.getValue()) //
+				.collect(Collectors.joining(",", "[", "]"));
+	}
+}
---
+++ src/test/java/com/soebes/katas/enums/TheEnumValuePatternTest.java
@@ -0,0 +1,79 @@
+package com.soebes.katas.enums;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+class TheEnumValuePatternTest {
+
+	@Test
+	void testName() {
+//		Arrays.stream(TheEnumValuePattern.values()).filter(s -> s.);
+	}
+
+	@Test
+	void firstValueShouldBeconverted() {
+		TheEnumValuePattern fromValue = TheEnumValuePattern.fromValue("BEGINNING-First");
+
+		assertThat(fromValue).isEqualTo(TheEnumValuePattern.FIRST);
+	}
+
+	@Test
+	void first() {
+		Field[] declaredFields = TheEnumValuePattern.Values.class.getDeclaredFields();
+
+		List<Field> collect = Arrays.stream(declaredFields) //
+				.filter(s -> !s.isSynthetic() && s.canAccess(null)) //
+				.collect(Collectors.toList());
+
+		collect.forEach(s -> {
+			try {
+				System.out.println(
+						"Field:" + s.getName() + " " + Modifier.isPublic(s.getModifiers()) + " V:" + s.get(null));
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+
+	}
+
+	private final Predicate<Field> FieldIsPublicStaticFinal = s -> //
+	Modifier.isPublic(s.getModifiers()) //
+			&& Modifier.isStatic(s.getModifiers()) //
+			&& Modifier.isFinal(s.getModifiers()) //
+	;
+
+	/**
+	 * Synthetic fields seemed to be generated during running of unit tests in
+	 * relationship with JaCoCo code coverage tool.
+	 */
+	private final Predicate<Field> FieldIsNotSynthentic = s -> !s.isSynthetic();
+
+	@Test
+	void first_a() {
+		Field[] declaredFields = TheEnumValuePattern.Values.class.getDeclaredFields();
+
+		List<Field> collect = Arrays.stream(declaredFields) //
+				.filter(s -> FieldIsNotSynthentic.test(s) && FieldIsPublicStaticFinal.test(s)) //
+				.collect(Collectors.toList());
+
+		collect.forEach(s -> System.out.println("Field:" + s.getName() + " " + Modifier.isPublic(s.getModifiers())));
+		
+	}
+
+	@Test
+	void third() {
+
+	}
+}
