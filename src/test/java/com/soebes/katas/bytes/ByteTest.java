9eaae07a60cf53bdd13e83def2d62338f9085fd1
Karl Heinz Marbaise <kama@soebes.de>
2019-03-03 21:44:05 +0100

    Improved.

---
+++ src/test/java/com/soebes/katas/bytes/ByteTest.java
@@ -0,0 +1,17 @@
+package com.soebes.katas.bytes;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+
+public class ByteTest {
+
+    @Test
+    void byteBufferTEst() {
+        int anInt = ByteBuffer.wrap(new byte[]{0, 1, 0, 0}).getInt();
+        System.out.println("anInt:" + anInt);
+
+        javax.crypto.spec.DHPublicKeySpec
+
+    }
+}
