8151f607d7da9837ad63be592630711ca383699b
Karl Heinz Marbaise <kama@soebes.de>
2019-09-07 23:04:16 +0200

    asdfadsfasdf

--- src/test/java/com/soebes/katas/factors/PrimeFactorsTest.java
+++ src/test/java/com/soebes/katas/factors/PrimeFactorsTest.java
@@ -1,102 +1,101 @@
 package com.soebes.katas.factors;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class PrimeFactorsTest
+ class PrimeFactorsTest
 {
     @Test
-    public void primeOfOneIsOne()
+     void primeOfOneIsOne()
     {
         assertThat( PrimeFactors.of( 1 ) ).isEqualTo( Collections.emptyList() );
     }
 
     @Test
-    public void primeOfTwoIsTwo()
+     void primeOfTwoIsTwo()
     {
         assertThat( PrimeFactors.of( 2 ) ).containsExactly( 2 );
     }
 
     @Test
-    public void primeOfThreeIsThree()
+     void primeOfThreeIsThree()
     {
         assertThat( PrimeFactors.of( 3 ) ).containsExactly( 3 );
     }
 
     @Test
-    public void primeOfFoureIsTwoTimesTwo()
+     void primeOfFoureIsTwoTimesTwo()
     {
         assertThat( PrimeFactors.of( 4 ) ).containsExactly( 2, 2 );
     }
 
     @Test
-    public void primeOfFiveIsFive()
+     void primeOfFiveIsFive()
     {
         assertThat( PrimeFactors.of( 5 ) ).containsExactly( 5 );
     }
 
     @Test
-    public void primeOfSixIsTwoTimesThree()
+     void primeOfSixIsTwoTimesThree()
     {
         assertThat( PrimeFactors.of( 6 ) ).containsExactly( 2, 3 );
     }
 
     @Test
-    public void primeOfSevenIsSeven()
+     void primeOfSevenIsSeven()
     {
         assertThat( PrimeFactors.of( 7 ) ).containsExactly( 7 );
     }
 
     @Test
-    public void primeOfEightIsTwoTimesTwoTimesTwo()
+     void primeOfEightIsTwoTimesTwoTimesTwo()
     {
         assertThat( PrimeFactors.of( 8 ) ).containsExactly( 2, 2, 2 );
     }
 
     @Test
-    public void primeOfNineIsThreeTimesThree()
+     void primeOfNineIsThreeTimesThree()
     {
         assertThat( PrimeFactors.of( 9 ) ).containsExactly( 3, 3 );
     }
 
     @Test
-    public void primeOfTenIsTwoTimesFive()
+     void primeOfTenIsTwoTimesFive()
     {
         assertThat( PrimeFactors.of( 10 ) ).containsExactly( 2, 5 );
     }
 
     @Test
-    public void primeOfElevenIsEleven()
+     void primeOfElevenIsEleven()
     {
         assertThat( PrimeFactors.of( 11 ) ).containsExactly( 11 );
     }
 
     @Test
-    public void primeOfTwelveIsTwoTimesTwoTimesThree()
+     void primeOfTwelveIsTwoTimesTwoTimesThree()
     {
         assertThat( PrimeFactors.of( 12 ) ).containsExactly( 2, 2, 3 );
     }
 
     @Test
-    public void primeOfTwoTimesTwoTimesThreeTimesThreeIsTwoTwoThreeThree()
+     void primeOfTwoTimesTwoTimesThreeTimesThreeIsTwoTwoThreeThree()
     {
         assertThat( PrimeFactors.of( 2 * 2 * 3 * 3 ) ).containsExactly( 2, 2, 3, 3 );
     }
 
     @Test
-    public void primeOf()
+     void primeOf()
     {
         assertThat( PrimeFactors.of( 5 * 5 * 7 * 7 ) ).containsExactly( 5, 5, 7, 7 );
     }
 
     @Test
-    public void primeOfXXX()
+     void primeOfXXX()
     {
         assertThat( PrimeFactors.of( 2 * 3 * 5 * 7 * 11 * 13 ) ).containsExactly( 2, 3, 5, 7, 11, 13 );
     }
-    
+
 }
