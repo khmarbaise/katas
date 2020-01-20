2dfaf40c7e4bc9f980540b547d1c0a24c29cbc5a
Karl Heinz Marbaise <kama@soebes.de>
2020-01-20 17:54:42 +0100

    First step with Deque.

---
+++ src/test/java/com/soebes/list/ListTypesTest.java
@@ -0,0 +1,16 @@
+package com.soebes.list;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+
+class ListTypesTest {
+
+  @Test
+  void firstList() {
+    LinkedList<String> ll = new LinkedList<>();
+    ArrayDeque<String> strings = new ArrayDeque<>();
+
+  }
+}
