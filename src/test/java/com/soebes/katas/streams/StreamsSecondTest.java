ad1b9bf36ce885bd7163d4762344686a3a290b0f
Karl Heinz Marbaise <kama@soebes.de>
2020-06-14 22:56:49 +0200

    Next step

--- src/test/java/com/soebes/katas/streams/StreamsSecondTest.java
+++ src/test/java/com/soebes/katas/streams/StreamsSecondTest.java
@@ -2,14 +2,14 @@ package com.soebes.katas.streams;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-public class StreamsSecondTest {
+import static java.util.stream.Collectors.toMap;
+
+class StreamsSecondTest {
 
 	@Test
 	void testName() {
@@ -19,10 +19,33 @@ public class StreamsSecondTest {
 		// k:1 v: 10*1
 
 		Map<Long, Long> result = IntStream.rangeClosed(0, 10)
-										  .mapToObj(v -> Long.valueOf(v))
-										  .collect(Collectors.toMap(kmap -> kmap, vmap -> 10L * vmap));
+				.mapToObj(v -> Long.valueOf(v))
+				.collect(toMap(kmap -> kmap, vmap -> 10L * vmap));
 
 		result.entrySet().forEach(e -> System.out.println("k:" + e.getKey() + " v:" + e.getValue()));
 
 	}
+
+	@Test
+	void second_test() {
+		// 0..10 =>
+		// k:0 v: 10*k
+		// k:1 v: 10*1
+		Map<Long, Long> result = IntStream.rangeClosed(0, 10)
+				.mapToObj(Long::valueOf)
+				.collect(valueMapperOnly(vmap -> 10L * vmap));
+
+		result.entrySet().forEach(e -> System.out.println("k:" + e.getKey() + " v:" + e.getValue()));
+	}
+
+	public static <T, K, U> Collector<T, ?, Map<K, U>> valueMapperOnly(Function<? super T, ? extends U> valueMapper) {
+		return toMap(Function.identity(), valueMapper);
+	}
+
+//	public static <T, K, U>
+//	Collector<T, ?, Map<K,U>> toMap(Function<? super T, ? extends K> keyMapper,
+//									Function<? super T, ? extends U> valueMapper) {
+//
+//
+//	}
 }
