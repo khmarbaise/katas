35780e43b20002e81fa7a261466301391e0b0476
Karl Heinz Marbaise <kama@soebes.de>
2020-06-17 22:51:48 +0200

    Improved.

--- src/test/java/com/soebes/katas/streams/StreamsSecondTest.java
+++ src/test/java/com/soebes/katas/streams/StreamsSecondTest.java
@@ -12,7 +12,7 @@ import static java.util.stream.Collectors.toMap;
 class StreamsSecondTest {
 
 	@Test
-	void testName() {
+	void key_identity() {
 
 		// 0..10 =>
 		// k:0 v: 10*k
@@ -27,25 +27,34 @@ class StreamsSecondTest {
 	}
 
 	@Test
-	void second_test() {
+	void value_identity_with_single() {
 		// 0..10 =>
 		// k:0 v: 10*k
 		// k:1 v: 10*1
 		Map<Long, Long> result = IntStream.rangeClosed(0, 10)
 				.mapToObj(Long::valueOf)
-				.collect(valueMapperOnly(vmap -> 10L * vmap));
+				.collect(toMapValue(vmap -> 10L * vmap));
 
 		result.entrySet().forEach(e -> System.out.println("k:" + e.getKey() + " v:" + e.getValue()));
 	}
 
-	public static <T, K, U> Collector<T, ?, Map<K, U>> valueMapperOnly(Function<? super T, ? extends U> valueMapper) {
-		return toMap(Function.identity(), valueMapper);
+	@Test
+	void key_identity_with_single() {
+		// 0..10 =>
+		// k:0 v: 10*k
+		// k:1 v: 10*1
+		Map<Long, Long> result = IntStream.rangeClosed(0, 10)
+				.mapToObj(Long::valueOf)
+				.collect(toMapKey(kmap -> kmap));
+
+		result.entrySet().forEach(e -> System.out.println("k:" + e.getKey() + " v:" + e.getValue()));
 	}
 
-//	public static <T, K, U>
-//	Collector<T, ?, Map<K,U>> toMap(Function<? super T, ? extends K> keyMapper,
-//									Function<? super T, ? extends U> valueMapper) {
-//
-//
-//	}
+	public static <T, K, U> Collector<T, ?, Map<K, U>> toMapValue(Function<? super T, ? extends U> valueMapper) {
+		return toMap ((Function<T, K>) Function.<K>identity(), valueMapper);
+	}
+	public static <T, K, U> Collector<T, ?, Map<K, U>> toMapKey(Function<? super T, ? extends K> keyMapper) {
+		return toMap (keyMapper, (Function<T,U>)Function.<U>identity());
+	}
+
 }
