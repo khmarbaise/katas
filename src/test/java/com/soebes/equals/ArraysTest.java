618cc556c31e690d661b4161df2ead46fe44e513
Karl Heinz Marbaise <kama@soebes.de>
2023-03-31 16:50:33 +0200

    Added some writing files examples.

---
+++ src/test/java/com/soebes/equals/ArraysTest.java
@@ -0,0 +1,60 @@
+package com.soebes.equals;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class ArraysTest {
+
+  @Test
+  void string_arrays() {
+    String[] first =  {"apple", "banana", "orange"};
+    String[] second = {"apple", "banana", "orange"};
+
+    var equals0 = first[0].equals(second[0]);
+    var equals1 = first[1].equals(second[1]);
+    var equals2 = first[2].equals(second[2]);
+
+    System.out.println("compare index 0 = " + equals0);
+    System.out.println("compare index 1 = " + equals1);
+    System.out.println("compare index 2 = " + equals2);
+
+    boolean sign0 = first[0] == second[0];
+    boolean sign1 = first[1] == second[1];
+    boolean sign2 = first[2] == second[2];
+
+    System.out.println("compare index 0 = " + sign0);
+    System.out.println("compare index 1 = " + sign1);
+    System.out.println("compare index 2 = " + sign2);
+  }
+
+  @Test
+  void compare_two_strings_with_equals() {
+    String s1 = "first";
+    String s2 = "first";
+
+    System.out.println("s1.equals(s2) = " + s1.equals(s2));
+    System.out.println("s1 == s2 " + (s1 == s2));
+  }
+
+  @Test
+  void compare_two_int_arrays() {
+    int[] first = {1, 2, 3, 1024,123456789};
+    int[] second = {1, 2, 3, 1024, 123456789};
+
+    boolean equals0 = first[0] == second[0];
+    boolean equals1 = first[1] == second[1];
+    boolean equals2 = first[2] == second[2];
+    boolean equals3 = first[3] == second[3];
+    boolean equals4 = first[4] == second[4];
+
+    System.out.println("equals0 = " + equals0);
+    System.out.println("equals1 = " + equals1);
+    System.out.println("equals2 = " + equals2);
+    System.out.println("equals3 = " + equals3);
+    System.out.println("equals4 = " + equals4);
+
+    var compare = Arrays.compare(first, second);
+    System.out.println("compare = " + compare);
+  }
+}
--- src/test/java/com/soebes/files/WriteFileTest.java
+++ src/test/java/com/soebes/files/WriteFileTest.java
@@ -2,10 +2,13 @@ package com.soebes.files;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 class WriteFileTest {
 
@@ -13,7 +16,34 @@ class WriteFileTest {
   void name() throws IOException {
 
     var file = Files.createFile(Path.of("test.txt"));
-    Files.writeString(file, "1. Zeile", StandardOpenOption.APPEND);
+    Files.write(file, List.of("Hello"));
+    Files.writeString(file, "1. Zeile");
     Files.writeString(file, "2. Zeile", StandardOpenOption.APPEND);
+  }
+
+  @Test
+  void another_way_to_write_to_a_file_list() throws IOException {
+    var path = Paths.get("hello.txt");
+    Files.write(path, List.of("Line1"));
+  }
+
+  @Test
+  void with_getbytes() throws IOException {
+    var path = Paths.get("hello.txt");
+    Files.write(path, "Hello".getBytes());
+  }
+
+  @Test
+  void another_way_to_write_to_a_file() throws IOException {
+    var path = Paths.get("hello.txt");
+    Files.writeString(path, "Hello");
+  }
+
+  @Test
+  void j7_style() throws IOException {
+    var path = Paths.get("hello.txt");
+    try (var bw = Files.newBufferedWriter(path)) {
+      bw.append("Hello");
+    }
   }
 }
